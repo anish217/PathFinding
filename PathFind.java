5ecfbdf9565aa07b3f1a182552e9b29a2e3b0ce2
anishmurthy <64456541+anishmurthy@users.noreply.github.com>
2020-05-15 18:04:15 -0400

    added basic button, no functionality

--- PathFind.java
+++ PathFind.java
@@ -1,17 +1,49 @@
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 
 public class PathFind {
-	
+	//set-up frame and panel
 	int HI=650;
 	int WID=800;
 	JFrame frame;
 	JPanel menu=new JPanel();
+	
+	//buttons
+	JButton searchB=new JButton("Search");
+	JButton resetB=new JButton("Reset");
+	JButton generateB=new JButton("Generate Map");
+	JButton clearB=new JButton("Clear Map");
+	
+	//algorithms and tools
+	String[] algos= {"A*","Dijkstra's"};
+	String[] tools= {"Start","End","Wall","Eraser"};
+	
+	//Drop Downs
+	JComboBox algoBox = new JComboBox(algos);
+	JComboBox toolBox = new JComboBox(tools);
+	
+	
+	//Labels
+	JLabel algoL = new JLabel("Algorithms");
+	JLabel toolL = new JLabel("Toolbox");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	Border bor=BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);;
 	
@@ -34,9 +66,46 @@ public class PathFind {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		//menu border
+		//menu set-up
+		menu.setLayout(null);
 		menu.setBorder(BorderFactory.createTitledBorder(bor, "Controls"));
-		menu.setBounds(10, 10, 220, 600);
+		menu.setBounds(10, 10, 220, 610);
+		int space=25;
+		int fromEdge=40;
+		int buff=45;
+		
+		
+		//BUTTONS
+		searchB.setBounds(fromEdge, space, 120, 25);
+		menu.add(searchB);
+		space+=buff;
+		
+		resetB.setBounds(fromEdge, space, 120, 25);
+		menu.add(resetB);
+		space+=buff;
+		
+		generateB.setBounds(fromEdge, space, 120, 25);
+		menu.add(generateB);
+		space+=buff;
+		
+		clearB.setBounds(fromEdge, space, 120, 25);
+		menu.add(clearB);
+		space+=40;
+		
+		//DROPDOWNS
+		algoL.setBounds(fromEdge, space, 120, 25);
+		menu.add(algoL);
+		space+=25;
+		algoBox.setBounds(fromEdge, space, 120, 25);
+		menu.add(algoBox);
+		space+=40;
+		
+		toolL.setBounds(fromEdge, space, 120, 25);
+		menu.add(toolL);
+		space+=25;
+		toolBox.setBounds(fromEdge, space, 120, 25);
+		menu.add(toolBox);
+		space+=40;
 		
 		
 		
